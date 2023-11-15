2cde91cefa9a023397a18b881cb222c6410b3614
AaronArredondo3 <Aaronarredondo63093@gmail.com>
2023-11-14 19:09:41 -0600

    updated fetch notes and swamson.

---
+++ Unit_4/Challenges/DisneyAPI/disneyFetch.js
@@ -0,0 +1,61 @@
+/* 
+    ! CHALLENGE
+        
+    - Create a boilerplate for index.html - Done
+        - Change the title to: "Disney API" - DONE
+        - link the disneyFetch.js at the bottom of the body element. DONE
+
+    - Create two different variables
+        - one will hold the URL provided - DONE
+        - one will generate a random number from 0-50 (whole number)- DONE
+    - Create an async function called INFO that will accept two different parameters - DONE
+        - the url
+        - the random number
+    - Within the function, create a TRY/CATCH.- DONE
+        - TRY and to fetch the url - DONE
+            - JSON-ify the data returned- DONE
+            - console.log 3 different positions:
+                - The overall promise returned 
+                    * hint: use the random number to target an object within the array provided.
+                - The name of a single character
+                - The imageURL of the character that is provided.
+        - CATCH any error that is returned.
+    - Invoke your function and pass with both variables as arguments.
+    
+    All results will be displayed in the console of the browser.
+
+    ------------------------------------------------------------------------------
+
+    API: Disney URL to use
+    https://api.disneyapi.dev/character
+    
+    
+    Additional Documentation:
+    https://disneyapi.dev/
+    */
+
+//! Global Variables
+const myUrl = 'https://api.disneyapi.dev/character';
+const randomNumber = Math.floor(Math.random() * 51);
+console.log(randomNumber);
+
+async function INFO(url, randomNumber) {
+    try {
+
+        let response = await fetch(url);
+        let data = await response.json();
+
+        console.log(data);
+        console.log(data[randomNumber].name);
+        console.log(data[randomNumber].imageURL);
+
+    }
+    catch(error) {
+
+        console.error(error);
+    }
+}
+
+INFO(myUrl, randomNumber)
+
+
--- Unit_4/FetchAPI/index.js
+++ Unit_4/FetchAPI/index.js
@@ -1,28 +1,29 @@
 /* 
-! API
+!   API
     - Application Programming Interface
     - Client (front end) to communicate with server (database)
-    - Allows HTTP requests and response lifecycles 
+    - Allows HTTP requests and response lifecycles
 */
 
 /* 
 !   Fetch
-    - Before fetch(), we would have to use an httpRequest through items like jQuery/Ajas
+    - Before fetch(), we would have to use an httpRequest through items like jQuery/Ajax
     - Is an API
-    - Is a method that requires 1 argument.
-        - The Location we are trying to obtain data from (url)
+    - Is a method that requires 1 argument. 
+        - The location we are trying to obtain data from (url)
     - Handles the request and response.
     - Returns a promise
-        - Allows us access to resolvers (.then() / .catch())
+        - Allows us access to resolvers (.then() / .catch() )
 */
 
 const testEndpoint = 'https://jsonplaceholder.typicode.com/posts/1';
 // console.log(fetch(testEndpoint));
 
-fetch(testEndpoint)
-// .then(res => console.log(res));
-// .then(res => console.log(res.url));
-// .then(res => console.log(res.body));
+// fetch(testEndpoint)
+// fetch('https://jsonplaceholder.typicode.com/posts/1')
+    // .then(res => console.log(res));
+    // .then(res => console.log(res.url));
+    // .then(res => console.log(res.body));
 
 /* 
 !   JSON
@@ -32,7 +33,7 @@ fetch(testEndpoint)
 
 *   Syntax
     {
-        "key": "value",
+        "key":"value",
         "key": true,
         "key": 0
     }
@@ -42,48 +43,47 @@ fetch(testEndpoint)
     - CANNOT hold comments or functions
 */
 
-
-fetch(testEndpoint) // provides us a Promise
+fetch(testEndpoint)  // provides us a Promise
     .then(response => response.json()) // Turns promise into a JSON
-    .then (data => {
+    .then(data => {
         // console.log(data); // Allows us to work with object
         displayIt(data.body);
     })
 
-    function displayIt(info) {
-        const body = document.querySelector('body');
-        const p = document.createElement('p');
-    
-        // console.log(info);
-        p.textContent = info.body;
-    
-        body.appendChild(p);
-    }
+function displayIt(info) {
+    const body = document.querySelector('body');
+    const p = document.createElement('p');
 
-    //* JSON.parse(string);
-    let myText = `{"name": "Gollum", "lost_ring": true}`;
-    console.log(myText);
-    let parseJSON = JSON.parse(myText);
-    console.log(parseJSON.name);
+    // console.log(info);
+    p.textContent = info;
 
-    const url = `https://meowfacts.herokuapp.com/`;
+    body.appendChild(p);
+}
 
-    // function getCatFacts() {
-    //     fetch(url)
-    //         .then(res => res.json())
-    //         .then(obj =>{
-    //             // console.log(obj);
-    //             // console.log(obj.data);
-    //             console.log(obj.data[0]);
-    //             displayIt(obj.data[0]);
-    //         })
-    // }
+//* JSON.parse(string);
+let myText = `{"name":"Gollum","lost_ring":true}`;
+console.log(myText);
+let parseJSON = JSON.parse(myText);
+console.log(parseJSON.name);
 
-    /* 
-    !   Error Handling
-        keywords:
-            - try
-            - catch
+const url = `https://meowfacts.herokuapp.com/`;
+
+// function getCatFacts() {
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(obj => {
+//             // console.log(obj);
+//             // console.log(obj.data);
+//             console.log(obj.data[0]);
+//             displayIt(obj.data[0]);
+//         })
+// }
+
+/* 
+!   Error Handling
+    Keywords:
+        - try
+        - catch
 
 *   Syntax
     try {
@@ -95,21 +95,21 @@ fetch(testEndpoint) // provides us a Promise
 
 const getCatFacts = async() => {
 
-        try{
-            let res = await fetch(url);
-            // let res = fetch(url);
-            let results = await res.json();
-            let data = results.data[0];
-            displayIt(data);
-        } catch (err){
-            console.error(err);
-        }
-
+    try {
+        let res = await fetch(url);
+        // let res = fetch(url);
+        let results = await res.json();
+        let data = results.data[0];
+        displayIt(data);
+    } catch (err) {
+        console.error(err);
     }
 
-    getCatFacts();
+}
 
-    //* Local Fetch
+getCatFacts();
+
+//* Local Fetch
 fetch('./local.json')
     .then(res => res.json())
     .then(data => {
