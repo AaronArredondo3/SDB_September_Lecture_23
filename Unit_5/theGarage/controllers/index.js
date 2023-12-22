9b0655213f529f3c408d58a76dc2ee70ad41366d
AaronArredondo3 <Aaronarredondo63093@gmail.com>
2023-12-21 21:22:19 -0600

    The Garage server/notes.

---
+++ Unit_5/theGarage/controllers/index.js
@@ -0,0 +1,6 @@
+const vehicleController = require('./vechicle.controller');
+const taskController = require('./task.controller');
+
+module.exports = {
+    vehicleController, taskController
+}
---
+++ Unit_5/theGarage/models/task.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Task = new mongoose.Schema({
+    title: String,
+    date: Date,
+    details: String,
+    resolved: Boolean,
+    vehicle_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "Vehicle"
+    }
+});
+
+module.exports = mongoose.model('Task', Task);
+
+/* 
+    - vehicle_id is using an ObjectId type as its data.
+    - In this case, referencing another collection (vehicle)
+*/
