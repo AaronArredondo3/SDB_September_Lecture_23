85ee0c644bd4182643a1eca3bd32e9026e0a63f6
AaronArredondo3 <Aaronarredondo63093@gmail.com>
2024-01-25 20:08:30 -0600

    Routes, useEffect, notes.

--- Unit_6/movie-client/src/App.jsx
+++ Unit_6/movie-client/src/App.jsx
@@ -1,10 +1,61 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Auth from './components/auth/Auth';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [ sessionToken, setSessionToken ] = useState('');
+
+  console.log('App:', sessionToken);
+
+  const updateToken = newToken => { // this will update the token in the browser
+    localStorage.setItem('token', newToken)
+    setSessionToken(newToken)
+  }
+
+  /* 
+  # useEffect()
+    - Allows us to perform side effects in functional components.
+    - Accepts two arguments
+      - function
+      - dependency
+        - optional
+        - Can denote a specific event to to trigger
+
+    EXAMPLES:
+    useEffect(() => {
+      Runs on every render
+    });
+
+    useEffect(() => {
+      Runs only on the first render
+    }, []);
+
+    useEffect(() => {
+        Runs on first render
+        Runs any time any dependency changes
+    }, [state]);
+  */
+
+    useEffect(() => { // when we use the useEffect is to control the flow
+      if(localStorage.getItem('token')){
+        setSessionToken(localStorage.getItem('token'));
+      }
+    }, [])
+
   return (
     <div className="App">
-      <Auth />
+      <Routes>
+        <Route 
+          path='/'
+          element={<Auth updateToken={updateToken} />}
+        />
+        <Route 
+          path='/movie'
+          element={<h2>Movie Section Placeholder</h2>} // this is the routes so it can move into the next page.
+        />
+      </Routes>
     </div>
   );
 }
